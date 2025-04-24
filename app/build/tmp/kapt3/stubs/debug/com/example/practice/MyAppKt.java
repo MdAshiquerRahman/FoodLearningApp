9d2decb7d34b231436dbac4948e3653b3e1cfe18
MdAshiquerRahman <t1ramd00@students.oamk.fi>
2025-04-24 19:53:04 +0600

    Updated

--- app/build/tmp/kapt3/stubs/debug/com/example/practice/MyAppKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/practice/MyAppKt.java
@@ -1,6 +1,6 @@
 package com.example.practice;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\"\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a*\u0010\u0000\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tH\u0007\u001a\b\u0010\n\u001a\u00020\u0001H\u0007\u00a8\u0006\u000b"}, d2 = {"MyApp", "", "modifier", "Landroidx/compose/ui/Modifier;", "viewModel", "Lcom/example/practice/viewmodel/AuthViewModel;", "videoViewModel", "Lcom/example/practice/viewmodel/VideoViewModel;", "context", "Landroid/content/Context;", "TopBar", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000(\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a2\u0010\u0000\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u000bH\u0007\u001a\b\u0010\f\u001a\u00020\u0001H\u0007\u00a8\u0006\r"}, d2 = {"MyApp", "", "modifier", "Landroidx/compose/ui/Modifier;", "viewModel", "Lcom/example/practice/viewmodel/AuthViewModel;", "videoViewModel", "Lcom/example/practice/viewmodel/VideoViewModel;", "context", "Landroid/content/Context;", "parentNavController", "Landroidx/navigation/NavController;", "TopBar", "app_debug"})
 public final class MyAppKt {
     
     @androidx.compose.runtime.Composable()
@@ -15,6 +15,7 @@ public final class MyAppKt {
     androidx.compose.ui.Modifier modifier, @org.jetbrains.annotations.NotNull()
     com.example.practice.viewmodel.AuthViewModel viewModel, @org.jetbrains.annotations.NotNull()
     com.example.practice.viewmodel.VideoViewModel videoViewModel, @org.jetbrains.annotations.NotNull()
-    android.content.Context context) {
+    android.content.Context context, @org.jetbrains.annotations.NotNull()
+    androidx.navigation.NavController parentNavController) {
     }
 }
